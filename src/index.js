9e2d3ffb124ddc1d2a2c9f2bf9c5e3a4d0042ea6
Jorge Lopes <jorgelopes@hey.com>
2021-03-20 15:34:04 +0100

    Message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+const e = require('express');
+
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+var usersAndPositions = [{ name: '', position: 1 }];
+
 io.on('connection', (socket) => {
   socket.on('messageUpdated', (msg) => {
+    const index = usersAndPositions.findIndex((obj) => {
+      obj.name = msg.name;
+    });
+    if (index) {
+      usersAndPositions[index].position = msg.position;
+    } else {
+      usersAndPositions.push({ name: msg.name, position: msg.position });
+    }
+    console.log(usersAndPositions);
     io.emit('textUpdated', msg);
   });
 });
