c27717ca67b6d6219a0fe69fe21871527595b3c3
Jorge Lopes <jorgelopes@hey.com>
2021-03-21 20:26:52 +0100

    Message

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-const e = require('express');
-
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 var usersAndPositions = [];
+setInterval(() => {
+  usersAndPositions = [];
+}, 10000);
 const checkForUserAndPosition = (msg) => {
+  // finds the position of the user in the array usersAndPositions
   const index = usersAndPositions.findIndex((obj) => {
     return obj.name === msg.user;
   });
-  console.log(index);
   if (index != -1) {
     usersAndPositions[index].position = msg.cursorPosition;
   } else {
     console.log('Pushing');
     usersAndPositions.push({ name: msg.user, position: msg.cursorPosition });
   }
-  console.log(usersAndPositions);
 };
 
 io.on('connection', (socket) => {
